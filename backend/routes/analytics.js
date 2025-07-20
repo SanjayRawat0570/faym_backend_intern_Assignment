526a1d96cc36a232d0bcee1841a8d57d99221525
Sanjay Rawat <sanjayrawat0570@gmail.com>
2025-07-20 23:57:27 +0530

    initial commit of analytics route

--- backend/routes/analytics.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getEventCount, getEventCountsByType } = require('../controllers/analyticsController');
-
-router.get('/event-counts', getEventCount);
-router.get('/event-counts-by-type', getEventCountsByType);
-
-module.exports = router;
